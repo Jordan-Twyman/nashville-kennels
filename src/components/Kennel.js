76bad5a455c20c24aff332a57001ecf3ddfae323
Jaytwy24 <jtwy24@gmail.com>
2021-12-01 10:08:02 -0500

    nav bar display for multiple veiws

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,50 +1,63 @@
+// import React from "react"
+// import { AnimalCard } from "./animal/AnimalCard"
+// import { CustomerCard } from "./customer/CustomerCard"
+// import { EmployeeCard } from "./employee/EmployeeCard"
+// import "./Kennel.css"
+// import { LocationCard } from "./location/LocationCard"
+// import { PropsAndState } from "./PropsAndState"
+
+
+
+
+
+// export const Kennel = () => (
+//     <>
+//         <h2>Nashville Kennels</h2>
+//         <small>Loving care when you're not there.</small>
+//         <address>
+//             <div>Visit Us at the Nashville North Location</div>
+//             <div>500 Puppy Way</div>
+//         </address>
+//         <PropsAndState yourName={"Jordan"} />
+//         <h2>Animals</h2>
+//         <article className="animals">
+//             <AnimalCard />
+//             <AnimalCard />
+//             <AnimalCard />
+//         </article>
+
+//         <h2>Customers</h2>
+//         <article className="customers">
+//             <CustomerCard />
+//             <CustomerCard />
+//             <CustomerCard />
+//             <CustomerCard />
+//         </article>
+
+//         <h2>Employees</h2>
+//         <article className="employees">
+//             <EmployeeCard />
+//             <EmployeeCard />
+//             <EmployeeCard />
+//         </article>
+
+//         <h2>Locations</h2>
+//         <article className="locations">
+//             <LocationCard />
+//             <LocationCard />
+//         </article>
+//     </>
+// )
+
+
 import React from "react"
-import { AnimalCard } from "./animal/AnimalCard"
-import { CustomerCard } from "./customer/CustomerCard"
-import { EmployeeCard } from "./employee/EmployeeCard"
+import { NavBar } from "./nav/NavBar"
+import { ApplicationViews } from "./ApplicationViews"
 import "./Kennel.css"
-import { LocationCard } from "./location/LocationCard"
-import { PropsAndState } from "./PropsAndState"
-
-
-
-
 
 export const Kennel = () => (
     <>
-        <h2>Nashville Kennels</h2>
-        <small>Loving care when you're not there.</small>
-        <address>
-            <div>Visit Us at the Nashville North Location</div>
-            <div>500 Puppy Way</div>
-        </address>
-        <PropsAndState yourName={"Jordan"} />
-        <h2>Animals</h2>
-        <article className="animals">
-            <AnimalCard />
-            <AnimalCard />
-            <AnimalCard />
-        </article>
-
-        <h2>Customers</h2>
-        <article className="customers">
-            <CustomerCard />
-            <CustomerCard />
-            <CustomerCard />
-            <CustomerCard />
-        </article>
-
-        <h2>Employees</h2>
-        <article className="employees">
-            <EmployeeCard />
-            <EmployeeCard />
-            <EmployeeCard />
-        </article>
-
-        <h2>Locations</h2>
-        <article className="locations">
-            <LocationCard />
-            <LocationCard />
-        </article>
+        <NavBar />
+        <ApplicationViews />
     </>
 )
