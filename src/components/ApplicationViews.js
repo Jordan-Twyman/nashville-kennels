05487041fa3feda9bf391fbffdcc6cd88f9a55ec
Jaytwy24 <jtwy24@gmail.com>
2021-12-01 10:13:32 -0500

    locations customers and employees nav link resources added

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,6 +2,9 @@ import React from "react"
 import { Route, Routes } from "react-router-dom"
 import { Home } from "./Home"
 import { AnimalCard } from "./animal/AnimalCard"
+import { LocationCard } from "./location/LocationCard"
+import { CustomerCard } from "./customer/CustomerCard"
+import { EmployeeCard } from "./employee/EmployeeCard"
 
 export const ApplicationViews = () => {
     return (
@@ -12,7 +15,14 @@ export const ApplicationViews = () => {
             {/* Render the animal list when http://localhost:3000/animals */}
             <Route path="/animals" element={<AnimalCard />}/>
            
+            <Route path="/customers" element={<CustomerCard />}/>
+
+            <Route path="/locations" element={<LocationCard />}/>
+
+            <Route path="/employees" element={<EmployeeCard />}/>
+
         </Routes>
     )
 }
+
 
